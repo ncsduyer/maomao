49f1b82c101ff55941d2192865b8dbdbc2c5f03a
libo <418431198@qq.com>
2019-06-19 17:33:21 +0800

    2019-6-19 1.0.0

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Header title="喵喵影院"/>
+        <div id="content">
+            <div class="cinema_menu">
+                <router-link tag="div" to="/cinema/ciList" class="city_switch">
+                    全城 <i class="iconfont icon-lower-triangle"></i>
+                </router-link>
+                <router-link tag="div" to="/cinema/ciList" class="brand_swtich">
+                    品牌 <i class="iconfont icon-lower-triangle"></i>
+                </router-link>
+                <router-link tag="div" to="/cinema/ciList" class="feature_switch">
+                    特色 <i class="iconfont icon-lower-triangle"></i>
+                </router-link>
+            </div>
+            <keep-alive>
+                <router-view/>
+            </keep-alive>
+        </div>
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header'
+    import TabBar from '@/components/TabBar'
+    export default {
+        name: "Cinema",
+        components:{Header,TabBar}
+    }
+</script>
+
+<style scoped>
+    #content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
